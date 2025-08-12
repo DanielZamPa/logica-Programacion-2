9e82fc094f12cad1cd8d5d033cf41407343604c8
Daniel Zamora Paez <[email>
2025-08-12 11:44:02 -0500

    Ejercicio completo, ejecucion por consola

--- temperaturas.js
+++ temperaturas.js
@@ -1,1 +1,15 @@
 const prompt = require("prompt-sync")();
+
+function conversionKelvin(celsius){
+    let kelvin = Number(celsius) + 273.15;
+    return kelvin;
+}
+function conversionFahrenheit(celsius){
+    let fahrenheit = (Number(celsius)*(9/5))+32
+    return fahrenheit;
+}
+
+let temperatura = prompt("Ingrese la temperatura en grados celsius: ");
+console.log("Temperatura en grdos kelvin: ", conversionKelvin(temperatura));
+console.log("Temperatura en grdos fahrenheit: ", conversionFahrenheit(temperatura));
+
